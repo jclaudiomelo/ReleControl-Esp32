3642902e9726de10a4f7948cc18547a19423e185
jclaudiomelo <claudiojcmn@yahoo.com.br>
2024-07-07 14:24:08 -0300

    v1.0.1

--- Rele8.ino
+++ Rele8.ino
@@ -15,10 +15,8 @@
 #define RELAY7_PIN 12 // GPIO12 (D6)
 #define RELAY8_PIN 14 // GPIO14 (D5)
 
-// Inicializando o display OLED com a biblioteca U8g2
 U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE, OLED_SCL, OLED_SDA);
 
-// Dados da rede Wi-Fi
 const char *ssid = "Rede_Oculta-srv";
 const char *password = "000000000";
 
@@ -32,14 +30,13 @@ static const unsigned char image_weather_humidity_white_bits[] U8X8_PROGMEM = { 
 
 static const unsigned char image_weather_temperature_bits[] U8X8_PROGMEM = { 0x38, 0x00, 0x44, 0x40, 0xd4, 0xa0, 0x54, 0x40, 0xd4, 0x1c, 0x54, 0x06, 0xd4, 0x02, 0x54, 0x02, 0x54, 0x06, 0x92, 0x1c, 0x39, 0x01, 0x75, 0x01, 0x7d, 0x01, 0x39, 0x01, 0x82, 0x00, 0x7c, 0x00 };
 
-static const unsigned char image_lamp_off_bits[] U8X8_PROGMEM = { 0x40, 0x00, 0xe0, 0x00, 0xa0, 0x00, 0x10, 0x01, 0x10, 0x01, 0x10, 0x01, 0x0c, 0x06, 0x02, 0x08, 0x01, 0x10, 0xfe, 0x0f, 0x10, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const unsigned char image_lamp_off_bits[] U8X8_PROGMEM =  { 0x40, 0x00, 0xe0, 0x00, 0xa0, 0x00, 0x10, 0x01, 0x10, 0x01, 0x10, 0x01, 0x0c, 0x06, 0x02, 0x08, 0x01, 0x10, 0xfe, 0x0f, 0x10, 0x01, 0xe0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
 static const unsigned char image_lamp_on_bits[] U8X8_PROGMEM = { 0x40, 0x00, 0xe0, 0x00, 0xa0, 0x00, 0x10, 0x01, 0x10, 0x01, 0x10, 0x01, 0x0c, 0x06, 0x02, 0x08, 0x01, 0x10, 0xfe, 0x0f, 0x10, 0x01, 0xe3, 0x18, 0x00, 0x00, 0x08, 0x02, 0x44, 0x04, 0x40, 0x00 };
 
 void setup() {
   Serial.begin(115200);
 
-  // Configurando os pinos dos relés como saída
   pinMode(RELAY1_PIN, OUTPUT);
   pinMode(RELAY2_PIN, OUTPUT);
   pinMode(RELAY3_PIN, OUTPUT);
@@ -49,7 +46,6 @@ void setup() {
   pinMode(RELAY7_PIN, OUTPUT);
   pinMode(RELAY8_PIN, OUTPUT);
 
-  // Inicializando os relés como desligados
   digitalWrite(RELAY1_PIN, LOW);
   digitalWrite(RELAY2_PIN, LOW);
   digitalWrite(RELAY3_PIN, LOW);
@@ -59,12 +55,11 @@ void setup() {
   digitalWrite(RELAY7_PIN, LOW);
   digitalWrite(RELAY8_PIN, LOW);
 
-  Wire.begin(OLED_SDA, OLED_SCL);  // Configurando as portas SDA e SCL
+  Wire.begin(OLED_SDA, OLED_SCL);
   u8g2.begin();
   u8g2.clearBuffer();
   u8g2.sendBuffer();
 
-  // Conectando ao Wi-Fi
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
@@ -78,24 +73,9 @@ void setup() {
   }
   Serial.println("Connected!");
   show_wifi_connected();
-  // Configurando servidor web
+
   server.on("/", handleRoot);
-  server.on("/r1/on", handleRelay1On);
-  server.on("/r1/off", handleRelay1Off);
-  server.on("/r2/on", handleRelay2On);
-  server.on("/r2/off", handleRelay2Off);
-  server.on("/r3/on", handleRelay3On);
-  server.on("/r3/off", handleRelay3Off);
-  server.on("/r4/on", handleRelay4On);
-  server.on("/r4/off", handleRelay4Off);
-  server.on("/r5/on", handleRelay5On);
-  server.on("/r5/off", handleRelay5Off);
-  server.on("/r6/on", handleRelay6On);
-  server.on("/r6/off", handleRelay6Off);
-  server.on("/r7/on", handleRelay7On);
-  server.on("/r7/off", handleRelay7Off);
-  server.on("/r8/on", handleRelay8On);
-  server.on("/r8/off", handleRelay8Off);
+  server.on("/relay", handleRelay);
   server.on("/status", handleStatus);
   server.begin();
   Serial.println("HTTP server started");
@@ -117,118 +97,40 @@ void show_wifi_connected() {
 }
 
 void handleRoot() {
-  server.send(200, "text/plain", "Server On");
+  server.send(200, "text/plain", "Relay Control");
 }
 
-void handleRelay1On() {
-  digitalWrite(RELAY1_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 1 ON");
-  Serial.println("Relay 1 ON");
-  updateDisplay();
-}
+void handleRelay() {
+  String relayParam = server.arg("id");
+  String action = server.arg("action");
 
-void handleRelay1Off() {
-  digitalWrite(RELAY1_PIN, LOW);
-  server.send(200, "text/plain", "Relay 1 OFF");
-  Serial.println("Relay 1 OFF");
-  updateDisplay();
-}
+  int relayPin;
+  if (relayParam == "1") relayPin = RELAY1_PIN;
+  else if (relayParam == "2") relayPin = RELAY2_PIN;
+  else if (relayParam == "3") relayPin = RELAY3_PIN;
+  else if (relayParam == "4") relayPin = RELAY4_PIN;
+  else if (relayParam == "5") relayPin = RELAY5_PIN;
+  else if (relayParam == "6") relayPin = RELAY6_PIN;
+  else if (relayParam == "7") relayPin = RELAY7_PIN;
+  else if (relayParam == "8") relayPin = RELAY8_PIN;
+  else {
+    server.send(400, "text/plain", "Rele Invalid");
+    return;
+  }
 
-void handleRelay2On() {
-  digitalWrite(RELAY2_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 2 ON");
-  Serial.println("Relay 2 ON");
-  updateDisplay();
-}
+  if (action == "on") {
+    digitalWrite(relayPin, HIGH);
+    server.send(200, "text/plain", "Relay " + relayParam + " ON");
+    Serial.println("Relay " + relayParam + " ON");
+  } else if (action == "off") {
+    digitalWrite(relayPin, LOW);
+    server.send(200, "text/plain", "Relay " + relayParam + " OFF");
+    Serial.println("Relay " + relayParam + " OFF");
+  } else {
+    server.send(400, "text/plain", "Invalid action");
+    return;
+  }
 
-void handleRelay2Off() {
-  digitalWrite(RELAY2_PIN, LOW);
-  server.send(200, "text/plain", "Relay 2 OFF");
-  Serial.println("Relay 2 OFF");
-  updateDisplay();
-}
-
-void handleRelay3On() {
-  digitalWrite(RELAY3_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 3 ON");
-  Serial.println("Relay 3 ON");
-  updateDisplay();
-}
-
-void handleRelay3Off() {
-  digitalWrite(RELAY3_PIN, LOW);
-  server.send(200, "text/plain", "Relay 3 OFF");
-  Serial.println("Relay 3 OFF");
-  updateDisplay();
-}
-
-void handleRelay4On() {
-  digitalWrite(RELAY4_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 4 ON");
-  Serial.println("Relay 4 ON");
-  updateDisplay();
-}
-
-void handleRelay4Off() {
-  digitalWrite(RELAY4_PIN, LOW);
-  server.send(200, "text/plain", "Relay 4 OFF");
-  Serial.println("Relay 4 OFF");
-  updateDisplay();
-}
-
-void handleRelay5On() {
-  digitalWrite(RELAY5_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 5 ON");
-  Serial.println("Relay 5 ON");
-  updateDisplay();
-}
-
-void handleRelay5Off() {
-  digitalWrite(RELAY5_PIN, LOW);
-  server.send(200, "text/plain", "Relay 5 OFF");
-  Serial.println("Relay 5 OFF");
-  updateDisplay();
-}
-
-void handleRelay6On() {
-  digitalWrite(RELAY6_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 6 ON");
-  Serial.println("Relay 6 ON");
-  updateDisplay();
-}
-
-void handleRelay6Off() {
-  digitalWrite(RELAY6_PIN, LOW);
-  server.send(200, "text/plain", "Relay 6 OFF");
-  Serial.println("Relay 6 OFF");
-  updateDisplay();
-}
-
-void handleRelay7On() {
-  digitalWrite(RELAY7_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 7 ON");
-  Serial.println("Relay 7 ON");
-  updateDisplay();
-}
-
-void handleRelay7Off() {
-  digitalWrite(RELAY7_PIN, LOW);
-  server.send(200, "text/plain", "Relay 7 OFF");
-  Serial.println("Relay 7 OFF");
-  updateDisplay();
-}
-
-void handleRelay8On() {
-  digitalWrite(RELAY8_PIN, HIGH);
-  server.send(200, "text/plain", "Relay 8 ON");
-  Serial.println("Relay 8 ON");
-  updateDisplay();
-}
-
-void handleRelay8Off() {
-  digitalWrite(RELAY8_PIN, LOW);
-  server.send(200, "text/plain", "Relay 8 OFF");
-  Serial.println("Relay 8 OFF");
   updateDisplay();
 }
 
@@ -288,6 +190,8 @@ void updateRelayImages() {
   } else {
     u8g2.drawXBMP(51, 47, 13, 16, image_lamp_off_bits);
   }
+
+  
 }
 
 void updateDisplay() {
